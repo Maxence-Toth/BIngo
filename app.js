de5b7728a3e525f93f9a1ca5e2c519fa7b72eb51
Moseus <toth.maxence@gmail.com>
2022-07-12 16:09:21 +0200

    AJout sur git du repo

---
+++ app.js
@@ -0,0 +1,39 @@
+let button1 = document.getElementById('button1')
+let button2 = document.getElementById('button2')
+let button3 = document.getElementById('button3')
+let button4 = document.getElementById('button4')
+let button5 = document.getElementById('button5')
+let button6 = document.getElementById('button6')
+let button7 = document.getElementById('button7')
+let button8 = document.getElementById('button8')
+
+function changecolor1(){
+button1.style.background = '#89C323'
+}
+function changecolor2(){
+button2.style.background = '#89C323'
+}
+function changecolor3(){
+button3.style.background = '#89C323'
+}
+function changecolor4(){
+button4.style.background = '#89C323'
+}
+function changecolor5(){
+button5.style.background = '#89C323'
+}
+function changecolor6(){
+button6.style.background = '#89C323'
+}
+function changecolor7(){
+button7.style.background = '#89C323'
+}
+function changecolor8(){
+button8.style.background = '#89C323'
+}
+
+function gg(){
+        alert("Allez il est temps d'aller en pause")
+    }
+
+
